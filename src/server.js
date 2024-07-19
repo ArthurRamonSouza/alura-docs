059a52c2781c88bde00e8d72ca026d051a507ef3
Eduardo Ramalho <131375484+eduardoramalhoz@users.noreply.github.com>
2024-07-19 14:49:25 -0300

    Starting a WebSocket project using Socket.io

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const currentPath = url.fileURLToPath(import.meta.url); // get current path
+const publicDir = path.join(path.dirname(currentPath), '../public'); // get public directory path
+
+app.use(express.static(publicDir)); // Use the public directory to serve static files
+
+const httpServer = http.createServer(app); // Create a HTTP server
+
+httpServer.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+const websocket = new Server(httpServer); // Create a websocket server
+
+websocket.on("connection", () => {
+    console.log("A user connected");
+});
